e68e5ebed2e6d1a85a4701d46f11c11b70518c72
Andrii Perun <andrewfreelan@gmail.com>
2020-06-15 10:39:50 +0300

    init

---
+++ app/Main.hs
@@ -0,0 +1,37 @@
+module Main where
+
+import qualified Data.ByteString
+import qualified Data.ByteString.Lazy
+import           Data.List
+import qualified Data.Map
+import           Data.Ord
+import           Huffman
+
+main :: IO ()
+main = do
+  firstFile <- readFile "resources/file1.txt"
+  secondFile <- readFile "resources/file2.txt"
+  
+  let unionFiles = (lines firstFile) ++ (lines secondFile)
+  let countedData = histogram (concat unionFiles)
+  let sortedData = sortBy (comparing swap) countedData
+  putStrLn $ show "sorted by number of unique elements"
+  putStrLn $ show sortedData
+  
+  let huffmanTree = sortedHuffman sortedData
+  putStrLn $ show "huffman tree"
+  let encoding = codes huffmanTree
+  
+  putStrLn $ show "encoded"
+  let encoded = map (encode encoding) unionFiles
+  mapM_ (print . showBits) encoded
+    
+  putStrLn $ show "encoded data in 'file.bin'"
+  let encBits0 = padToEight (concat encoded)
+  let bits = bitpack encBits0
+  Data.ByteString.Lazy.writeFile "file.bin" bits
+  let Right encBits1 = bitunpack . Data.ByteString.pack . Data.ByteString.Lazy.unpack $ bits
+  
+  let decoded = map (decode huffmanTree) encoded
+  putStrLn $ show "decoded"
+  putStrLn $ show decoded
